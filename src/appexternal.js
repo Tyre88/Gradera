2b3d64151f019bb726bb6af5dd8ec89f5a7ce9ef
Tyre88 <victor@webbdudes.se>
2015-11-08 10:38:50 +0100

    Adding loading to the external views aswell.

--- src/appexternal.js
+++ src/appexternal.js
@@ -1,12 +1,22 @@
 LoadCss(["content/css/external.css"]);
 (function(angular) {
-    angular.module("graderaklubbexternal", ['ng', 'ngRoute', 'ui.router', 'ngMaterial'])
+    angular.module("graderaklubbexternal", ['ng', 'ngRoute', 'ui.router', 'ngMaterial', 'angular-loading-bar'])
         .controller('home', homeController);
+
+    angular.module("graderaklubbexternal").config(externalConfig);
+
+    externalConfig.$inject = ["cfpLoadingBarProvider"];
 
     function homeController() {
         var vm = this;
         vm.Test = "Hello world";
         console.log("LALA");
+    }
+
+    function externalConfig(cfpLoadingBarProvider) {
+        cfpLoadingBarProvider.includeSpinner = true;
+        cfpLoadingBarProvider.latencyThreshold = 250;
+        cfpLoadingBarProvider.spinnerTemplate = '<div class="loader md-whiteframe-z1">Laddar...</div>';
     }
 
     $(document).ready(function () {
