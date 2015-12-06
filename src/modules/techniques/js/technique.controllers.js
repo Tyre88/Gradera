0bdd63758b9a0db61ff13c71576509260e6e74b0
Tyre88 <victor@webbdudes.se>
2015-12-06 09:48:21 +0100

    Fixing so we can see the whole image on a technique.

--- src/modules/techniques/js/technique.controllers.js
+++ src/modules/techniques/js/technique.controllers.js
@@ -2,14 +2,15 @@
     LoadCss('modules/techniques/css/technique.css');
     angular.module('graderaklubb').controller('showtechnique', showtechniqueController);
 
-    showtechniqueController.$inject = ["$state", "$stateParams", "techniqueService"];
+    showtechniqueController.$inject = ["$state", "$stateParams", "techniqueService", "$mdDialog"];
 
-    function showtechniqueController($state, $stateParams, techniqueService) {
+    function showtechniqueController($state, $stateParams, techniqueService, $mdDialog) {
         var vm = this;
         vm.TechniqueId = ~~$stateParams.id;
         vm.Technique = {};
 
         vm.GetTechnique = GetTechnique;
+        vm.ViewImage = ViewImage;
 
         function GetTechnique() {
             if(vm.TechniqueId > 0)
@@ -22,6 +23,31 @@
             }
         }
 
+        function ViewImage(image) {
+            $mdDialog.show({
+                template: "<md-dialog-content class='image-dialog-content'><img src='" + image + "' /></md-dialog-content><md-dialog-actions class='image-dialog-actions'><md-button ng-click='vm.CloseImage();' class='md-fab'><i class='fa fa-close'></i></md-button></md-dialog-actions>",
+                parent: angular.element(document.body),
+                controller: "showtechniqueimage",
+                controllerAs: "vm",
+                clickOutsideToClose: true,
+                fullscreen: true
+            });
+        }
+
         vm.GetTechnique();
+    }
+
+    angular.module('graderaklubb').controller('showtechniqueimage', showtechniqueimageController);
+
+    showtechniqueimageController.$inject = ["$mdDialog"];
+
+    function showtechniqueimageController($mdDialog) {
+        var vm = this;
+
+        vm.CloseImage = CloseImage;
+
+        function CloseImage() {
+            $mdDialog.hide();
+        }
     }
 }(window.angular));
