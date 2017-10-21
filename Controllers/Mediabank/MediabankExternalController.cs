76e6d59762cde2b61890f0edd2a1e0ab50c7e113
Tyre88 <victor@webbdudes.se>
2017-10-21 15:24:43 +0200

    #53 Adding external controller for mediabank

---
+++ Controllers/Mediabank/MediabankExternalController.cs
@@ -0,0 +1,39 @@
+﻿using Gradera.Entities.Mediabank;
+using Gradera.Mediabank.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+
+namespace Gradera_Klubb.Controllers.Mediabank
+{
+    public class MediabankExternalController : ApiController
+    {
+        private IMediabank _mediabank;
+
+        public MediabankExternalController(IMediabank mediabank)
+        {
+            _mediabank = mediabank;
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetFile(string fileGUID)
+        {
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            MediabankEntity mediabankFile = _mediabank.GetFile(fileGUID);
+            if (mediabankFile != null && mediabankFile.IsExternal)
+            {
+                response.Content = new ObjectContent<MediabankEntity>(mediabankFile, new JsonMediaTypeFormatter());
+            }
+            else
+            {
+                response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            }
+
+            return response;
+        }
+    }
+}
